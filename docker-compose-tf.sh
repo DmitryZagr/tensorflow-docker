4e11cb02e50f9c7c29d9e78381642063cb282f75
DmitryZagr <bikemaster2000@list.ru>
2019-10-07 21:43:15 +0300

    init

---
+++ docker-compose-tf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -   
+
+if [ -n "$1" ]
+then
+    command=$1
+    container_name=tensorflow
+
+    if [ "$command" == "up" ]; then
+        docker run -d  --name $container_name --restart=always --gpus all --memory=58g --cpus=7  -p 8888:8888  -v $(realpath data):/tf/notebooks-persistent -v $(realpath config/jupyter_notebook_config.json):/root/.jupyter/jupyter_notebook_config.json:ro tensorflow/tensorflow:2.0.0-gpu-py3-jupyter
+    elif [ "$command" == "down" ]; then
+        docker stop $container_name
+        docker rm $container_name
+    elif [ "$command" == "up-basic" ]; then
+        docker run -d  --name $container_name --restart=always --gpus all --memory=58g --cpus=7  -p 8888:8888  -v $(realpath data):/tf/notebooks-persistent -v $(realpath config/jupyter_notebook_config.json):/root/.jupyter/jupyter_notebook_config.json:ro tensorflow/tensorflow:2.0.0-py3-jupyter
+    elif [ "$command" == "down-basic" ]; then
+        docker stop $container_name
+        docker rm $container_name
+    fi
+else
+    echo "No parameters found. "
+fi
